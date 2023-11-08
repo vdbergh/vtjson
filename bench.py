78a525fa050c583cf541157345c63f14984a32b5
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2023-11-08 18:27:23 +0100

    import validate -> import vtjson in bench.py

--- bench.py
+++ bench.py
@@ -4,7 +4,7 @@ from numbers import Real  # matches int and float
 from timeit import timeit
 
 from bson.objectid import ObjectId
-from validate import ip_address, regex, union, url, validate
+from vtjson import ip_address, regex, union, url, validate
 
 # This schema only matches new runs.
 
