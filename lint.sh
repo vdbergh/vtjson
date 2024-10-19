ca42a2cdb01b714f89067207446259f571baef04
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-10-19 20:43:08 +0200

    Rename some files.

--- lint.sh
+++ lint.sh
@@ -2,6 +2,6 @@
 
 black *.py
 isort --profile black *.py
-flake8 --max-line-length 88 bench.py test_validate.py vtjson.py
+flake8 --max-line-length 88 bench.py test_vtjson.py vtjson.py
 mdl *.md
 cat README.md | aspell -a --mode=markdown --personal=./ignore.txt |grep \&
