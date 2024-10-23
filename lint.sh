8bb84ec60149f3f485890fa5651ca36128711773
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-10-23 06:20:17 +0200

    Enable checking for redundant casts in actions.

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mypy test_vtjson.py vtjson.py --strict --explicit-package-bases --no-warn-redundant-casts
+mypy test_vtjson.py vtjson.py --strict --explicit-package-bases
 black *.py
 isort --profile black *.py
 flake8 --max-line-length 88 bench.py test_vtjson.py vtjson.py
