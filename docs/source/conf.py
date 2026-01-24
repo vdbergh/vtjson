75f574fc664e880f3cd692d9aa889ddf9acf4f00
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2026-01-24 10:59:37 +0100

    Update version number also in docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ add_module_names = False
 project = "vtjson"
 copyright = "2024-2026, Michel Van den Bergh"
 author = "Michel Van den Bergh"
-release = "2.2.6"
+release = "2.2.7"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
