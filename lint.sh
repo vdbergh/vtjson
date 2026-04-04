c8aaea529a6ef3854e8c99203264ebcb11efa497
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2026-04-04 11:06:13 +0200

    Add ty test to lint.sh. Work in progress.

--- lint.sh
+++ lint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 mypy bench.py test_vtjson.py vtjson.py --strict --explicit-package-bases
+rm -rf env
+python -m venv env
+env/bin/python -m pip install dnspython email_validator idna python-magic typing_extensions
+ty check --python env/bin/python --python-version 3.14 vtjson.py
 black *.py
 isort --profile black *.py
 flake8 --max-line-length 88 bench.py test_vtjson.py vtjson.py
