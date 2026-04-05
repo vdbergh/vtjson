168b4eb13e24e22138df38010695b51d16e75d40
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2026-04-05 07:08:05 +0200

    Also perform ty check for bench.py in lint.sh

--- lint.sh
+++ lint.sh
@@ -5,7 +5,7 @@ PYTHON=env/bin/python
 python -m venv env
 $PYTHON -m pip -q install black isort flake8 ty mypy pymongo dnspython email_validator idna python-magic typing_extensions  --upgrade
 $PYTHON -m mypy bench.py test_vtjson.py vtjson.py --strict --explicit-package-bases
-$PYTHON -m ty check --python env/bin/python --python-version 3.12 vtjson.py test_vtjson.py
+$PYTHON -m ty check --python env/bin/python --python-version 3.12 bench.py vtjson.py test_vtjson.py
 $PYTHON -m black --target-version py312 *.py
 $PYTHON -m isort --profile black *.py
 $PYTHON -m flake8 --max-line-length 88 bench.py test_vtjson.py vtjson.py
